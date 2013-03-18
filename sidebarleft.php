a13a2c79a6797e478d59cc056a146b1a64c9abb1
Tom Hughes <tom@compton.nu>
2013-03-18 17:48:54 +0000

    Add customised theme for State of the Map 2011

--- sidebarleft.php
+++ sidebarleft.php
@@ -2,36 +2,76 @@
 
 <div id="sidebarleft">
 
+<div style="margin-left:2px;text-align:center">
 
-<div style="margin-left:10px;">
+<p align="center">State Of The Map would not be possible without the generous support from our sponsors:</p>
 
+<p align="center"><b>Tungsten Carbide</b></p>
 
-<!-- I N S E R T   Y O U R   A D   C O D E   H E R E
-erase the following lines starting with <img ... and insert your adsense or other ad code here 
-Works best with 160x600 px ads, formatted with white background and black text color
-if you don't want to display ads, erase the following lines starting with <img ... and put some widgets (design/widgets) into the left sidebar!
--->
+<p align="center"><a href="http://www.mapquest.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/01/mapquest.png" alt="MapQuest"></a></p>
 
-<img src="<?php bloginfo('url'); ?>/wp-content/themes/aerodrome/images/sidead.png" alt="Sidead" />
-<img src="<?php bloginfo('url'); ?>/wp-content/themes/aerodrome/images/sidead.png" alt="Sidead" />
+<p align="center"><b>Platinum</b></p>
 
+<p align="center"><a href="http://www.en.logiball.de/openstreetmap.html"><img src="http://2009.stateofthemap.org/wp-content/uploads/2009/07/logo_slogan_trans_160.png" alt="LOGIBALL"></a></p>
 
+<p align="center"><a href="http://www.bing.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/06/bing_small.png" alt="Bing"></a></p>
+
+<p align="center"><b>Gold</b></p>
+
+<p align="center"><a href="http://code.google.com/soc/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/05/gsoc2011.png" alt="Google SOC 2011"></a></p>
+
+<p align="center"><a href="http://www.deloitte.com/view/en_US/us/index.htm"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/08/deloitte-logo-160px.jpg" alt="Deloitte"></a></p>
+
+<p align="center"><a href="http://www.worldbank.org/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/09/worldbank_160px.png" alt="World Bank"></a></p>
+
+<p align="center"><a href="http://usgif.org/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/09/us_gif_160px.png" alt="USGIF"></a></p>
+
+<p align="center"><b>Silver</b></p>
+
+<p align="center"><a href="http://www.esri.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/08/esri_160px.jpg" alt="Esri"></a></p>
+
+<p align="center"><a href="http://www.waze.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/08/waze_160px.jpg" alt="Waze"></a></p>
+
+<p align="center"><a href="http://www.moonshadowmobile.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/08/moonshadow_160px.png" alt="Moonshadow"></a></p>
+
+<p align="center"><b>Bronze</b></p>
+
+<p align="center"><a href="http://trails.lamouroux.de/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/07/trails-small.png" alt="Trails"></a></p>
+
+<p align="center"><a href="http://ulmon.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/07/ulmon_small.png" alt="Ulmon"></a></p>
+
+<p align="center"><a href="http://www.iosphere.de/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/07/iosphere_small.png" alt="iosphere"></a></p>
+
+<p align="center"><a href="http://www.fasterimaging.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/09/faster_imaging_160px.png" alt="Faster Imaging"></a></p>
+
+<p align="center"><b>Partners</b></p>
+
+<p align="center"><a href="http://www.visitdenver.org/OSM"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/03/VISITDENVER_vert.jpg" alt="Visit Denver"></a></p>
+
+<p align="center"><a href="http://www.ucdenver.edu/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/04/ucd.png" alt="University of Colorado Denver"></a></p>
+
+<p align="center"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/08/edge_160px.jpg" alt="EDGE"></p>
+
+<p align="center"><a href="http://www.directionsmag.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/02/directionsmagazine.png" alt="Directions Magazine"></a></p>
+
+<p align="center"><a href="http://www.gisuser.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/02/gisuser50.gif" alt="GISuser"></a></p>
+
+<p align="center"><a href="http://www.linuxpromagazine.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/02/LinuxProMagazine_WebLogo1.jpg" alt="Linux Pro Magazine"></a></p>
+
+<p align="center"><a href="http://www.smart-developer.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/02/smart_developer.jpg" alt="Smart Developer"></a></p>
+
+<p align="center"><a href="http://www.profsurv.com/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/05/psm150px.png" alt="Professional Surveyor Magazine"></a></p>
+
+<p align="center"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/03/CTO-Logo.jpg" alt="Colorado Tourism Office"></p>
+
+<p align="center"><a href="http://slashgeo.org/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/08/slashgeo_160px.png" alt="Slashgeo"></a></p>
+
+<p align="center">Consider supporting <br />State Of The Map?<br /> <a href="http://2011.stateofthemap.org/sponsors/">Click here</a>.</p>
+
+<p align="center" style="color:#888;">Curious who sponsored last years edition?<br /> <a href="http://2010.stateofthemap.org/" style="color:#888;">Visit the 2010 website</a>.</p>
+
+<p align="center"><br /><br />FOSS4G 2011 we also be held in Denver, right after SOTM11. We encourage you to attend both events!<br /><a href="http://2011.foss4g.org/"><img src="http://2011.stateofthemap.org/wp-content/uploads/2011/05/foss4g_web_block_150x.png" alt="Foss4G"></a></p>
 
 </div>
-
-
-<ul>
-<?php         /* Widgetized sidebar, if you have the plugin installed. */
-                if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Left Sidebar") ) : ?>
-
-
-
-<?php endif; ?>
-
-
-
-
-
-</ul>
 
 </div><!-- end sidebarleft -->
